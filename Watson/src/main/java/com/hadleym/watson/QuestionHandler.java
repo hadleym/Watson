7b7ab4aa103bc828222a1c0bc3e6519a02b1f5ef
Mark Hadley <hadleym@email.arizona.edu>
2017-05-06 18:06:34 -0700

    Comments

--- Watson/src/main/java/com/hadleym/watson/QuestionHandler.java
+++ Watson/src/main/java/com/hadleym/watson/QuestionHandler.java
@@ -6,21 +6,27 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+// This class is the interface between the QueryHelper and
+// the Question class.
 public class QuestionHandler {
+	// ignore "POTPOURRI" as a category in jeopardy.
 	String[] ignore = { "POTPOURRI" };
 	File questionsFile;
 	public ArrayList<Question> questions;
 
+	// analyzes the Question file.
 	public QuestionHandler(File filename) {
 		questionsFile = filename;
 		questions = new ArrayList<Question>();
 		try {
 			createQuestions();
-		} catch (IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	// reads through the file and creates questions
+	// to then be used by the QueryHelper class.
 	public void createQuestions() throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(questionsFile));
 		for (String subject = br.readLine(); subject != null; subject = br.readLine()) {
@@ -33,10 +39,11 @@ public class QuestionHandler {
 			}
 
 			questions.add(new Question(subject, br.readLine(), br.readLine()));
+
 			// read blank line
 			br.readLine();
 		}
 		br.close();
 	}
-	
+
 }
